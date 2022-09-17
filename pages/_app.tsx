e993e6f1a663ab259f5f134545de3e227eb8cdec
Luisotee <luisotaviomgr1@gmail.com>
2022-09-17 19:41:53 -0300

    Setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,37 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
+import "@fontsource/rubik";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Credit Card</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <MantineProvider
+        theme={{
+          fontFamily: "Rubik, sans-serif",
+          shadows: {
+            md: "1px 1px 3px rgba(0, 0, 0, .25)",
+            xl: "5px 5px 3px rgba(0, 0, 0, .25)",
+          },
+          headings: {
+            fontFamily: "Rubik, sans-serif",
+          },
+        }}
+        withGlobalStyles
+        withNormalizeCSS
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
